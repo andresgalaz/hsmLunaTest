d6ed97c239946eb043d09ed87782389f761fb5c5
Andres Galaz <andres.galaz@c2c.cl>
2021-01-15 14:50:11 -0300

    wrapping

--- src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
@@ -5,30 +5,37 @@ import static java.lang.System.out;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.KeyFactory;
+import java.security.Key;
 import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.security.interfaces.RSAPrivateCrtKey;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Enumeration;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+
+import com.safenetinc.luna.LunaUtils;
+import com.safenetinc.luna.provider.key.LunaKey;
+
 public class LoadPrivateKey {
+	//
+	// AES block size is 128 bits or 16 bytes. Used easy HEX values so you can plug
+	// them
+	// into a website like http://aes.online-domain-tools.com/ to verify encrypted
+	// values.
+	//
+	private static final IvParameterSpec FIXED_128BIT_IV_FOR_TESTS = new IvParameterSpec(
+			LunaUtils.hexStringToByteArray("DEADD00D8BADF00DDEADBABED15EA5ED"));
+
 	public static void main(String[] args) throws Exception {
-		// Load input stream into keystore
+		HsmManager.login();
+		// Input file keystore
 		File fJks = new File("/home/firmador/keys/keystore.jks");
-		// String password = "serverpwd";
 		FileInputStream is = new FileInputStream(fJks);
 
-        HsmManager.login();
 		out.println("Load from file");
-        KeyStore ks = HsmManager.loadKey(is);
-		// KeyStore ks = KeyStore.getInstance("Luna");
-		out.println("Load NULL NULL");
-		ks.load(null, null);
-
-		// out.println("Load IS PASSWORD");
-		// ks.load(is, password.toCharArray());
+		KeyStore ks = HsmManager.loadKey(is);
 
 		// List the aliases
 		Enumeration<String> aliases = ks.aliases();
@@ -45,20 +52,23 @@ public class LoadPrivateKey {
 			out.println(b);
 		}
 
-		PrivateKey privateKey = get("/mnt/hdd/agalaz/Descargas/localhost.key");
-		String cModulus = ((RSAPrivateCrtKey) privateKey).getModulus().toString();
-		String cExponent = ((RSAPrivateCrtKey) privateKey).getPublicExponent().toString();
-		out.println(cModulus);
-		out.println(cExponent);
+		Path path = Paths.get("/home/firmador/keys/wrappedKeyBin.b64");
+		byte[] wrappedKey = Files.readAllBytes(path);
+
+		String password = "serverpwd";
+		final Key kek = (PrivateKey) ks.getKey("localhost", password.toCharArray());
+
+		Cipher lunaAesCbcCipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "LunaProvider");
+		//
+		// Unwrap the key
+		//
+		lunaAesCbcCipher.init(Cipher.UNWRAP_MODE, kek, FIXED_128BIT_IV_FOR_TESTS);
+		LunaKey unwrappedKey = (LunaKey) lunaAesCbcCipher.unwrap(wrappedKey, "AES", Cipher.SECRET_KEY);
+		out.println("Unwrapped key (in clear same as original): " + getHex(unwrappedKey.getEncoded()));
 	}
 
-	public static PrivateKey get(String filename) throws Exception {
-
-		byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
-
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-		KeyFactory kf = KeyFactory.getInstance("RSA");
-
-		return kf.generatePrivate(spec);
+	private static String getHex(byte array[]) {
+		return "0x" + LunaUtils.getHexString(array, false).toUpperCase();
 	}
+
 }
