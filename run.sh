f82260011a9efb7fcec55e8989a0e841b2ff8d04
Firmador C2C <firmador@sign-c2c.digital.gob.cl>
2021-01-15 10:38:08 -0300

    wrapping

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# java -cp lib/*:build/classes/main to.noc.hsm.lunasa.example.KeyWrappingExample
+# java -cp lib/*:build/classes/main to.noc.hsm.lunasa.example.WrappedKeySerializationExample
+java -cp lib/*:build/classes/main to.noc.hsm.lunasa.example.KeyWrappingWithHsmGeneratedKeysExample
---
+++ runSetPartitionProperties.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp lib/*:build/classes/main to.noc.hsm.lunasa.example.HsmManager
