d8288745e6bbf2e574ff0f0d4e3fea3625cfa398
Andres Galaz <andres.galaz@gmail.com>
2021-01-21 11:51:32 -0300

    wrapping

--- src/main/java/to/noc/hsm/lunasa/example/SavePrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/SavePrivateKey.java
@@ -1,37 +1,79 @@
 package to.noc.hsm.lunasa.example;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.spec.PKCS8EncodedKeySpec;
 import static java.lang.System.out;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.Enumeration;
+
 public class SavePrivateKey {
+	private Certificate certificate;
+	private PrivateKey privateKey;
+	private String alias;
 
 	public static void main(String[] args) throws Exception {
-		if (args.length != 1) {
-			out.println("Se esperaba un parámetro como nombre de archivo");
+		if (args.length != 2) {
+			out.println("Se esperaban dos un parámetros: archivo y password");
 			return;
 		}
+		SavePrivateKey me = new SavePrivateKey();
+
 		HsmManager.login();
-		Key k = getPrivateKey(args[0]);
-		HsmManager.saveKey("agalaz", k);
+		me.loadCertificado(args[0], args[1]);
+		out.println("alias:" + me.getAlias());
+		out.println("privateKey:" + me.getPrivateKey());
+		out.println("certificate:" + me.getCertificate());
+		HsmManager.saveRsaKey(me.getAlias(), me.getPrivateKey(), new Certificate[] {me.getCertificate()});
 		HsmManager.logout();
 	}
 
-	public static PrivateKey getPrivateKey(String filename) throws Exception {
+	public void loadCertificado(String filename, String clave) throws KeyStoreException, NoSuchAlgorithmException,
+			CertificateException, IOException, UnrecoverableKeyException {
 		File f = new File(filename);
 		FileInputStream fis = new FileInputStream(f);
-		DataInputStream dis = new DataInputStream(fis);
-		byte[] keyBytes = new byte[(int) f.length()];
-		dis.readFully(keyBytes);
-		dis.close();
-		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-		KeyFactory kf = KeyFactory.getInstance("RSA");
-		return kf.generatePrivate(spec);
+
+		KeyStore p12 = KeyStore.getInstance("pkcs12");
+		p12.load(fis, clave.toCharArray());
+		fis.close();
+
+		Enumeration<String> e = p12.aliases();
+		while (e.hasMoreElements()) {
+			setAlias( e.nextElement());
+		}
+		setPrivateKey((PrivateKey) p12.getKey(getAlias(), clave.toCharArray()));
+		setCertificate(p12.getCertificate(getAlias()));
+	}
+
+	public Certificate getCertificate() {
+		return certificate;
+	}
+
+	public void setCertificate(Certificate certificate) {
+		this.certificate = certificate;
+	}
+
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+
+	public void setPrivateKey(PrivateKey privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
 	}
 
 }
