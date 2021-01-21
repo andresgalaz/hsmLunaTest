ea3534e4ddab7fa62318216d541fc6e93ec0003e
Andres Galaz <andres.galaz@gmail.com>
2021-01-21 12:54:43 -0300

    wrapping

--- src/main/java/to/noc/hsm/lunasa/example/SavePrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/SavePrivateKey.java
@@ -5,19 +5,14 @@ import static java.lang.System.out;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.security.Key;
-import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPrivateCrtKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPrivateKeySpec;
 import java.util.Enumeration;
 
 import com.safenetinc.luna.provider.key.LunaKey;
@@ -42,47 +37,22 @@ public class SavePrivateKey {
 		out.println("alias:" + me.getAlias());
 		out.println("privateKey[" + me.getPrivateKey().getClass().getName() + "]:");
 		me.print(me.getPrivateKey());
-		
-		// RSAPrivateCrtKey rsaKey = (RSAPrivateCrtKey) me.getPrivateKey();
-		// String cModulus = rsaKey.getModulus().toString();
-		// String cExponent = rsaKey.getPublicExponent().toString();
-		// me.setPrivateKey(createPrivateKey(rsaKey.getModulus(), rsaKey.getPublicExponent()));
 
 		// Limpia
 		HsmManager.deleteKey(me.getAlias());
 		// Graba
 		HsmManager.saveRsaKey(me.getAlias(), me.getPrivateKey(), new Certificate[] { me.getCertificate() });
-		// HsmManager.saveKey(me.getAlias(), me.getPrivateKey());
 		// Recupera
 		LunaPrivateKeyRsa kHsm = (LunaPrivateKeyRsa) HsmManager.getSavedKey(me.getAlias());
-		out.println("kHsm[" + me.getPrivateKey().getClass().getName() + "]:");
-		out.println(kHsm.IsKeyPersistent());
-		out.println(kHsm.getAlgorithm());
-		out.println(kHsm.GetAlias());
-		out.println(kHsm.getSlot());
-		// out.println(kHsm.getUsageCount());
-		// out.println(kHsm.getUsageLimit());
-		out.println(kHsm.getModulus());
-		try {
-			out.println(kHsm.getPrivateExponent());
-		} catch (Exception e) {
-			out.println("No se pudo obtener Exponen:" + e.getMessage());
-		}
+		out.println("kHsm[" + kHsm.getClass().getName() + "]:");
 		Key kLoc = LunaKey.LocateKeyByAlias(me.getAlias(), 0);
 		out.println("kLoc[" + kLoc.getClass().getName() + "]:");
-		
-		LunaKey lunaKey = kHsm;
-		out.println("lunaKey[" + me.getPrivateKey().getClass().getName() + "]:");
-		out.println(lunaKey.IsKeyPersistent());
-		out.println(lunaKey.getAlgorithm());
-		out.println(lunaKey.GetAlias());
-		out.println(lunaKey.getSlot());
-		// out.println(lunaKey.getUsageCount());
-		// out.println(lunaKey.getUsageLimit());
-		out.println(lunaKey);
 
-		// me.print(kHsm);
 		HsmManager.logout();
+
+		KeyStore keyStore = KeyStore.getInstance("Luna");
+		keyStore.load(null, null);
+
 	}
 
 	public void loadCertificado(String filename, String clave) throws KeyStoreException, NoSuchAlgorithmException,
@@ -110,18 +80,6 @@ public class SavePrivateKey {
 		out.println("Exponent:" + cExponent);
 	}
 
-//	private static PrivateKey createPrivateKey(String cModulus, String cPrivateExponent)
-//			throws NoSuchAlgorithmException, InvalidKeySpecException {
-//		return createPrivateKey(new BigInteger(cModulus), new BigInteger(cPrivateExponent));
-//	}
-
-	private static PrivateKey createPrivateKey(BigInteger modulus, BigInteger privateExponent)
-			throws NoSuchAlgorithmException, InvalidKeySpecException {
-		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-		RSAPrivateKeySpec rsaPrivateKeySpec = new RSAPrivateKeySpec(modulus, privateExponent);
-		return keyFactory.generatePrivate(rsaPrivateKeySpec);
-	}
-	
 	public Certificate getCertificate() {
 		return certificate;
 	}
