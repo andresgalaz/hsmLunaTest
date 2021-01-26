9b311a5d20844743462f6b7a11367f6958ce26bc
Andres Galaz <andres.galaz@gmail.com>
2021-01-25 21:18:56 -0300

    wrapping

--- src/main/java/to/noc/hsm/lunasa/example/KeyWrappingExample.java
+++ src/main/java/to/noc/hsm/lunasa/example/KeyWrappingExample.java
@@ -106,15 +106,15 @@ public class KeyWrappingExample {
 
 		// Start software cipher operation
 		Cipher sunJceCipher = Cipher.getInstance("DESede/ECB/NoPadding", "SunJCE");
-		// sunJceCipher.init(Cipher.ENCRYPT_MODE, unwrapped3DesKey);
-		sunJceCipher.init(Cipher.ENCRYPT_MODE, kek);
+		sunJceCipher.init(Cipher.ENCRYPT_MODE, unwrapped3DesKey);
+		// sunJceCipher.init(Cipher.ENCRYPT_MODE, kek);
 		byte[] cipherText = sunJceCipher.doFinal(plainText.getBytes());
 		out.println("SunJCE encryption result:\n\t" + getHex(cipherText));
 
 		// Start Luna HSM cipher operation
 		Cipher lunaHsmCipher = Cipher.getInstance("DESede/ECB/NoPadding", "LunaProvider");
-		// lunaHsmCipher.init(Cipher.DECRYPT_MODE, unwrapped3DesKey);
-		lunaHsmCipher.init(Cipher.DECRYPT_MODE, kek);
+		lunaHsmCipher.init(Cipher.DECRYPT_MODE, unwrapped3DesKey);
+		// lunaHsmCipher.init(Cipher.DECRYPT_MODE, kek);
 		byte[] originalClearText = lunaHsmCipher.doFinal(cipherText);
 		out.println("LunaProvider HSM decrypt result:\n\t" + new String(originalClearText));
 
