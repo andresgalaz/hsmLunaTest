95a68300aa7bdcc98cab881c3dbe502a114350c1
Andres Galaz <andres.galaz@gmail.com>
2021-01-27 16:48:58 -0300

    unwrapping

--- src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
@@ -2,122 +2,53 @@ package to.noc.hsm.lunasa.example;
 
 import static java.lang.System.out;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
-import java.util.Enumeration;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.SecretKey;
+
+import org.apache.axis.encoding.Base64;
 
 import com.safenetinc.luna.LunaUtils;
-import com.safenetinc.luna.provider.key.LunaKey;
 
 public class LoadPrivateKey {
-	//
-	// AES block size is 128 bits or 16 bytes. Used easy HEX values so you can plug
-	// them
-	// into a website like http://aes.online-domain-tools.com/ to verify encrypted
-	// values.
-	//
-	private static final IvParameterSpec FIXED_128BIT_IV_FOR_TESTS = new IvParameterSpec(
-			LunaUtils.hexStringToByteArray("DEADD00D8BADF00DDEADBABED15EA5ED"));
+	private static final String KEK_ALIAS = "MSP_WK";
 
 	public static void main(String[] args) throws Exception {
 		HsmManager.login();
 
-		KeyStore ks = loadFromFile("/home/firmador/keys/keystore.jks");
+		String bin = loadFromFile("/home/firmador/keys/bin.b64");
+		String material = loadFromFile("/home/firmador/keys/material.b64");
 
-		out.println("Load wrapped from file [BIN]");
-		Path path = Paths.get("/home/firmador/keys/wrappedKeyBin.b64");
-		byte[] wrappedKey = Files.readAllBytes(path);
+		HsmManager.login();
+		HsmManager.setSecretKeysExtractable(true);
+		SecretKey wmk = (SecretKey) HsmManager.getSavedKey(KEK_ALIAS);
 
-		String password = "serverpwd";
-		// final Key kek = (PrivateKey) ks.getKey("localhost", password.toCharArray());
-		final Key kek = (PrivateKey) ks.getKey("localhost", password.toCharArray());
-		Cipher lunaAesCbcCipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "LunaProvider");
-		// Cipher lunaAesCbcCipher = Cipher.getInstance("RSA/CBC/PKCS5Padding", "LunaProvider");
-		//
-		// Unwrap the key
-		//
-		out.println("Init cipher usando kek:" + kek);
-		lunaAesCbcCipher.init(Cipher.UNWRAP_MODE, kek, FIXED_128BIT_IV_FOR_TESTS);
-		out.println("Unwrapping ...");
-		LunaKey unwrappedKey = (LunaKey) lunaAesCbcCipher.unwrap(wrappedKey, "AES", Cipher.SECRET_KEY);
-		out.println("Unwrapped key (in clear same as original): " + getHex(unwrappedKey.getEncoded()));
+		Cipher cipher = Cipher.getInstance("RSAAESWrap", "LunaProvider");
+		cipher.init(Cipher.WRAP_MODE, wmk);
+		cipher.init(Cipher.UNWRAP_MODE, wmk);
+		Key unwrappedExtractableKey = cipher.unwrap(bin.getBytes(), "AES", Cipher.SECRET_KEY);
+		out.println(getHex(unwrappedExtractableKey.getEncoded()));
+
+		Key unwrappedExtractableKey2 = cipher.unwrap(material.getBytes(), "AES", Cipher.SECRET_KEY);
+		out.println(getHex(unwrappedExtractableKey2.getEncoded()));
+
 	}
 
 	private static String getHex(byte array[]) {
 		return "0x" + LunaUtils.getHexString(array, false).toUpperCase();
 	}
 
-	private static KeyStore loadFromFile(String cNombreAcrh)
+	private static String loadFromFile(String cNombreAcrh)
 			throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException {
-		// Input file keystore
-		File fJks = new File(cNombreAcrh);
-		FileInputStream is = new FileInputStream(fJks);
-
-		out.println("Load from file:" + cNombreAcrh);
-		KeyStore sr = KeyStore.getInstance("JKS"); ;
-		sr.load(is, "serverpwd".toCharArray());
-		
-		KeyStore ks = sr; // HsmManager.loadKey(is);
-		is.close();
-
-		out.println("Lista Alias");
-		out.println("===========================================");
-		// List the aliases
-		Enumeration<String> aliases = ks.aliases();
-		for (; aliases.hasMoreElements();) {
-			String alias = aliases.nextElement();
-
-			// Does alias refer to a private key?
-			boolean b = ks.isKeyEntry(alias);
-
-			// Does alias refer to a trusted certificate?
-			b = ks.isCertificateEntry(alias);
-
-			out.print(alias + ":");
-			out.println(b);
-		}
-		out.println("===========================================");
-		return ks;
+		String contentB64 = new String(Files.readAllBytes(Paths.get(cNombreAcrh)));
+		String content = new String(Base64.decode(contentB64), "UTF-8");
+		return content;
 	}
-
-	@SuppressWarnings("unused")
-	private static void Unwrapp(KeyStore ks, String cNombreArch)
-			throws IOException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException,
-			NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
-		out.println("Load wrapped from file:" + cNombreArch);
-		Path path = Paths.get(cNombreArch);
-		byte[] wrappedKey = Files.readAllBytes(path);
-
-		String password = "serverpwd";
-		// final Key kek = (PrivateKey) ks.getKey("localhost", password.toCharArray());
-		final Key kek = (PrivateKey) ks.getKey("localhost", HsmManager.getPartitionPass().toCharArray());
-		Cipher lunaAesCbcCipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "LunaProvider");
-		//
-		// Unwrap the key
-		//
-		out.println("Init cipher usando kek:" + kek);
-		lunaAesCbcCipher.init(Cipher.UNWRAP_MODE, kek, FIXED_128BIT_IV_FOR_TESTS);
-		out.println("Unwrapping ...");
-		LunaKey unwrappedKey = (LunaKey) lunaAesCbcCipher.unwrap(wrappedKey, "AES", Cipher.SECRET_KEY);
-		out.println("Unwrapped key (in clear same as original): " + getHex(unwrappedKey.getEncoded()));
-	}
-
 }
