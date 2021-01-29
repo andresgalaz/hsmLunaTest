3219ea9994d403088c2aa4ddc8ff6f3f8cc0e059
Andres Galaz <andres.galaz@gmail.com>
2021-01-29 17:37:40 -0300

    unwrapping

--- src/main/java/to/noc/hsm/lunasa/example/WrapPrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/WrapPrivateKey.java
@@ -58,7 +58,7 @@ public class WrapPrivateKey {
 		// Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256ANDMGF1Padding",
 		// "LunaProvider");
 		// Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding", "BC");
-		Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding", "BC");
+		Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding", "LunaProvider");
 		// cipher.init(Cipher.WRAP_MODE, wmk);
 		// AlgorithmParameters algParams = AlgorithmParameters.getInstance("IV",
 		// "LunaProvider");
