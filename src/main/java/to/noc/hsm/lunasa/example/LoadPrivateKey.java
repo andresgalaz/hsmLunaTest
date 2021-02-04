5eba2515e4441fbbeb83f0326fe255435c36cd53
Andres Galaz <andres.galaz@gmail.com>
2021-02-04 16:53:16 -0300

    wrapping

--- src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
+++ src/main/java/to/noc/hsm/lunasa/example/LoadPrivateKey.java
@@ -104,7 +104,7 @@ public class LoadPrivateKey {
 //		Cipher.RSA/*/NoPadding
 //		Cipher.RSA/*/OAEPWithSHA1AndMGF1Padding
 //		Cipher.RSA/*/PKCS1v1_5 
-		Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding", "LunaProvider");
+		Cipher cipher = Cipher.getInstance("DES3/CBC/NoPadding", "LunaProvider");
 		AlgorithmParameters algParams = AlgorithmParameters.getInstance("IV", "LunaProvider");
 		algParams.init(new IvParameterSpec(new byte[8]));
 		try {
