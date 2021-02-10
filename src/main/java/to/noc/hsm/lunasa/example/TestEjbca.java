501be41ea22824de5438dc94e9a701c5729cd974
Andres Galaz <andres.galaz@gmail.com>
2021-02-10 18:26:44 -0300

    test ejbca

--- src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
+++ src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
@@ -1,7 +1,10 @@
 package to.noc.hsm.lunasa.example;
 
 import java.net.URL;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.xml.namespace.QName;
 
 import org.apache.log4j.Logger;
@@ -16,6 +19,19 @@ public class TestEjbca {
 	// private static String host = "https://ejbca/";
 
 	public static void main(String[] args) throws Exception {
+        /* Start of Fix */
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }   
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+
+        } };
 
 		CryptoProviderTools.installBCProvider();	
 		String urlstr = host+ "ejbca/ejbcaws/ejbcaws?wsdl";
