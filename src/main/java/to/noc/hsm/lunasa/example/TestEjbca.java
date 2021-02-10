29bf29d1e60643bc682f50acc1fcc9db9d30532b
Andres Galaz <andres.galaz@gmail.com>
2021-02-09 22:50:50 -0300

    test ejbca

--- src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
+++ src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
@@ -4,26 +4,34 @@ import java.net.URL;
 
 import javax.xml.namespace.QName;
 
+import org.apache.log4j.Logger;
 import org.cesecore.util.CryptoProviderTools;
 import org.ejbca.core.protocol.ws.client.gen.EjbcaWS;
 import org.ejbca.core.protocol.ws.client.gen.EjbcaWSService;
 
 public class TestEjbca {
+	private static Logger   logger = Logger.getLogger( TestEjbca.class );
+	
 	private static String host = "https://10.77.40.5:8443/";
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws Exception {
 
 		CryptoProviderTools.installBCProvider();	
 		String urlstr = host+ "ejbca/ejbcaws/ejbcaws?wsdl";
-			
+		logger.info("Paso 1");
 		System.setProperty("javax.net.ssl.trustStore","/home/firmador/keys/truststore.jks");
 		System.setProperty("javax.net.ssl.trustStorePassword","");  
 		System.setProperty("javax.net.ssl.keyStore","/home/firmador/keys/keystore.jks");
 		System.setProperty("javax.net.ssl.keyStorePassword","serverpwd");      
 		                             
+		logger.info("Paso 2");
 		QName qname = new QName("http://ws.protocol.core.ejbca.org/", "EjbcaWSService");
+		logger.info("Paso 3");
 		EjbcaWSService service = new EjbcaWSService(new URL(urlstr),qname);
+		logger.info("Paso 4");
 		EjbcaWS ejbcaraws = service.getEjbcaWSPort();  
+		logger.info("Paso 5");
 		
 	}
 
