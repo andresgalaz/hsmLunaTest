13de08e1f83de5f6edc0d18930911a74deb02605
Andres Galaz <andres.galaz@gmail.com>
2021-02-10 18:32:56 -0300

    test ejbca

--- src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
+++ src/main/java/to/noc/hsm/lunasa/example/TestEjbca.java
@@ -1,8 +1,11 @@
 package to.noc.hsm.lunasa.example;
 
 import java.net.URL;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import javax.xml.namespace.QName;
@@ -13,25 +16,40 @@ import org.ejbca.core.protocol.ws.client.gen.EjbcaWS;
 import org.ejbca.core.protocol.ws.client.gen.EjbcaWSService;
 
 public class TestEjbca {
-	private static Logger   logger = Logger.getLogger( TestEjbca.class );
-	
+	private static Logger logger = Logger.getLogger(TestEjbca.class);
+
 	private static String host = "https://10.77.40.5/";
 	// private static String host = "https://ejbca/";
 
+    // Permite usar HTTPS sin bajar el certificado localmente
+    static {
+        TrustManager[] trustAllCertificates = new TrustManager[] {
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    // No need to implement.
+                }
+
+                public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    // No need to implement.
+                }
+            }
+        };
+
+        try {
+            SSLContext sc = SSLContext.getInstance( "SSL" );
+            sc.init( null, trustAllCertificates, new SecureRandom() );
+            HttpsURLConnection.setDefaultSSLSocketFactory( sc.getSocketFactory() );
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError( e );
+        }
+    }
+
+	
 	public static void main(String[] args) throws Exception {
-        /* Start of Fix */
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }   
-
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-
-        } };
 
 		CryptoProviderTools.installBCProvider();	
 		String urlstr = host+ "ejbca/ejbcaws/ejbcaws?wsdl";
